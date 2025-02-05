a66a08174b152c2b82ce6e5f95ac7bd9984c8df6
donsfak <falibetasoro@gmail.com>
2025-02-05 02:23:37 +0000

    review de l'app & fichiers gradle!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,5 +21,14 @@ class MyApp extends StatelessWidget {
                 zoom: 14)),
       ),
     );
+  }
+}
+
+Future<void> requestLocationPermission() async {
+  var status = await Permission.location.request();
+  if (status.isGranted) {
+    print("Permission accordée");
+  } else {
+    print("Permission refusée");
   }
 }
