218253242f2cb30d68d88c06f6ebd048dda054c1
donsfak <falibetasoro@gmail.com>
2025-02-09 02:10:56 +0000

    connecting to supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, non_constant_identifier_names, unused_field, prefer_final_fields
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -16,16 +17,83 @@ enum AppState {
   postRide,
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  AppState _appState = AppState.choosingLocation;
+  LatLng? _currentLocation;
+  CameraPosition? _initialCameraPosition;
+  late GoogleMapController _mapController;
+  @override
+  void initState() {
+    requestLocationPermission();
+    _checkLocationPermission();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> _checkLocationPermission() async {
+    final isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Veuillez activer votre service de localisation'),
+        ));
+      }
+    }
+    var Permission = await Geolocator.checkPermission();
+    if (Permission == LocationPermission.denied) {
+      Permission = await Geolocator.requestPermission();
+      if (Permission == LocationPermission.denied) {
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Veuillez activer votre service de localisation'),
+          ));
+          return;
+        }
+      }
+    }
+    if (Permission == LocationPermission.deniedForever) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Veuillez activer votre service de localisation'),
+        ));
+        return;
+      }
+    }
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _currentLocation = LatLng(position.latitude, position.longitude);
+      _initialCameraPosition = CameraPosition(
+        target: _currentLocation!,
+        zoom: 14,
+      );
+    });
+    _mapController
+        .animateCamera(CameraUpdate.newCameraPosition(_initialCameraPosition!));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: GoogleMap(
-            initialCameraPosition: CameraPosition(
-                target: LatLng(37.42796133580664, -122.085749655962),
-                zoom: 14)),
+          myLocationEnabled: true,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(37.42796133580664, -122.085749655962), zoom: 14),
+          onMapCreated: (controller) {
+            _mapController = controller;
+          },
+        ),
       ),
     );
   }
